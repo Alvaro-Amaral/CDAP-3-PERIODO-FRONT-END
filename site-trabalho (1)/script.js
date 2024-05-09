64dcdb6ae0c189cc692c74ee9761b77bbea0cc91
mateus26guerra <mateus26guerra@gmail.com>
2024-05-09 19:45:16 -0300

    segunda atualizcao do site por mateus

--- site-trabalho (1)/script.js
+++ site-trabalho (1)/script.js
@@ -3,3 +3,9 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 500)
 })
+
+
+
+function toggle(){
+    document.querySelector('.texto').classList.toggle('visivel')
+}
