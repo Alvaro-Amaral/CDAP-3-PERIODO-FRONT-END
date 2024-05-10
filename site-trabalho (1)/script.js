f56cfaf71e1a96e9e516501cc24d6df6d405c168
mateus26guerra <mateus26guerra@gmail.com>
2024-05-10 00:52:23 -0300

    atualizacao 3

--- site-trabalho (1)/script.js
+++ site-trabalho (1)/script.js
@@ -9,3 +9,26 @@ window.addEventListener("scroll", function(){
 function toggle(){
     document.querySelector('.texto').classList.toggle('visivel')
 }
+
+////////////curiculo///////////////
+
+let indiceImagem = 0;
+
+function trocarConteudo(delta) {
+  const imagens = ["imagem/a.jpg", "imagem/b.jpg", "imagem/c.jpg"];
+  const textos = [
+    { titulo: "Aluno: mateus guerra feitosa", descricao: "matricular 01625297 " },
+    { titulo: "Título do Texto 2", descricao: "Texto descritivo 2..." },
+    { titulo: "Título do Texto 3", descricao: "Texto descritivo 3..." }
+  ];
+
+  // Avança ou retrocede o índice da imagem
+  indiceImagem = (indiceImagem + delta + imagens.length) % imagens.length;
+
+  // Atualiza o texto
+  document.getElementById("titulo").innerText = textos[indiceImagem].titulo;
+  document.getElementById("descricao").innerText = textos[indiceImagem].descricao;
+
+  // Atualiza a imagem
+  document.getElementById("imagem").src = imagens[indiceImagem];
+}
