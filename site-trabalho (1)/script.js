4d7238d35dabc82d1833ce1602c536e2599d2484
Anderkato <129921043+Anderkato@users.noreply.github.com>
2024-05-17 02:26:47 -0300

    add
    foi adicionado infos dos membros, mudança na cor,tamanho e arrendodamento da caixa de email e algumas alteraçoes nas cores

--- site-trabalho (1)/script.js
+++ site-trabalho (1)/script.js
@@ -17,12 +17,12 @@ let indiceImagem = 0;
 function trocarConteudo(delta) {
   const imagens = ["imagem/mateus.jpeg", "imagem/caio.jpeg", "imagem/alvaro.jpeg ","imagem/dino.jpeg","imagem/lucas.jpeg","imagem/aden.jpeg"];
   const textos = [
-    { titulo: "MATEUS GUERRA FEITOSA ", descricao: " matricular 01625297  \n\nvvvfffffffffffffffffffffffffffffffffffffffffffffffffffffff\nvvvfffffffffffffffffffffffffffffffffffffffffffffffffffffff\nvvvvvvffffvvvffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffv\nvvvffffffffffffffffffffffffffffffffffffffffffffffffffff" },
-    { titulo: "CAIO VINICIUS ARAUJO", descricao: "Texto descritivo 2..." },
-    { titulo: "ÁLVARO AMARAL OLIVEIRA", descricao: "Texto descritivo 3..." },
-    { titulo: "LUCAS BERNADINO SILVESTRE", descricao: "Texto descritivo 4..." },
-    { titulo: "LUCAS BOMFIM DO NASCIMENTO", descricao: "Texto descritivo 5..." },
-    { titulo: "ANDERSON JOSÉ SÁTIRO DE OLIVEIRA BARBOSA", descricao: "Matricula:01624883\nUninassau Graças\nTurma:3MD\nCurso:Ciência da Computação\nTurno:Manhã" }
+    { titulo: "MATEUS GUERRA FEITOSA ", descricao: " Matricula: 01625297\nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" },
+    { titulo: "CAIO VINICIUS ARAUJO", descricao: "Matricula: 01611224\nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" },
+    { titulo: "ÁLVARO AMARAL OLIVEIRA", descricao: "Matricula: \nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" },
+    { titulo: "LUCAS BERNADINO SILVESTRE", descricao: "Matricula: \nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" },
+    { titulo: "LUCAS BOMFIM DO NASCIMENTO", descricao: "Matricula: \nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" },
+    { titulo: "ANDERSON JOSÉ SÁTIRO DE OLIVEIRA BARBOSA", descricao: "Matricula: 01624883\nUninassau Graças\nTurma: 3MD\nCurso: Ciência da Computação\nTurno: Manhã" }
   ];
 
   // Avança ou retrocede o índice da imagem
