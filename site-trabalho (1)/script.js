c08877f21c917efae13b45f64045f4acad9af969
Anderkato <129921043+Anderkato@users.noreply.github.com>
2024-05-29 23:44:09 -0300

    pagina imagem

--- site-trabalho (1)/script.js
+++ site-trabalho (1)/script.js
@@ -1,7 +1,7 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 300)
+    header.classList.toggle('rolagem', window.scrollY > 1)
 })
 
 
